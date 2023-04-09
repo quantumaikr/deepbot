d9a90d7dc5df675941666b05836fc270ee123c5a
이강훈 <bruce@iganghun-ui-MacBookPro.local>
2023-04-09 17:13:46 +0900

    gradio 기본옵션 수정

--- collect.sh
+++ collect.sh
@@ -2,8 +2,8 @@
 
 num_process=10 # The number of processes to collect data
 max_total_tokens=500000 # Set maximum numbers of tokens to collect data
-api_key=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 # Set your openai api key
-for ((i=0; i<$num_process; i++))
+api_key=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 # Set your openai api key
+for ((i=0; i<5; i++))
 do
     python3 collect.py $api_key $max_total_tokens $i $num_process quora &
 done
--- demo/app.py
+++ demo/app.py
@@ -242,5 +242,5 @@ demo.title = "Baize"
 if __name__ == "__main__":
     reload_javascript()
     demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-        share=False, favicon_path="./assets/favicon.ico", inbrowser=True
+        share=True, favicon_path="./assets/favicon.ico", inbrowser=True
     )
